28e7f442775d59151fdaa363cba0f4d1ae39cc30
jacktea88 <jacktea@gmail.com>
2025-07-19 23:05:52 +0800

    新增 Post 模型及初始遷移檔案

--- 02mblog/mblog/mysite/models.py
+++ 02mblog/mblog/mysite/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, )
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now=False, auto_now_add=True, )
+    slug = models.SlugField(max_length=50, )
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ["-pub_date", ]
+
+    def __str__(self):
+        # pass
+        return self.title
