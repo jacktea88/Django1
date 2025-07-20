399b91b75ba7e62d52bbb05fdd9c1cd5faf8eea1
jacktea88 <jacktea@gmail.com>
2025-07-20 21:59:38 +0800

    新增post model在後台的顯示欄位，並新增唐詩模型及示例數據

--- 02mblog/mblog/mysite/admin.py
+++ 02mblog/mblog/mysite/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from mysite.models import Post
 
 # Register your models here.
+# admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date', 'body', )
+
+# 記得拿掉上面的註冊，否則會報錯(重覆註冊)
+admin.site.register(Post, PostAdmin)
+
