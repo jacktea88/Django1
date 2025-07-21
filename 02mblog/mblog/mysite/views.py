23ab97c0379324287e12543dc56773862d032fd5
jacktea88 <jacktea@gmail.com>
2025-07-21 12:23:18 +0800

    更新設定檔以包含模板目錄，並新增首頁模板以顯示最新文章

--- 02mblog/mblog/mysite/views.py
+++ 02mblog/mblog/mysite/views.py
@@ -9,6 +9,8 @@ def homepage(request):
     for count, post in enumerate(posts):
         post_list.append(f"Post {count}: {post} <br> {post.body} <br>")
         print(post)
-    return HttpResponse(post_list)    
     # return HttpResponse("Hello World!")
+    # return HttpResponse(post_list)
+    
     # return render(request, 'homepage.html', {'posts': Post.objects.all()})
+    return render(request, 'index.html', locals())
