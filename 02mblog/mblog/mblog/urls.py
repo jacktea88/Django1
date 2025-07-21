831f3d471809606092ec076418af305415a48a45
jacktea88 <jacktea@gmail.com>
2025-07-21 11:34:19 +0800

    新增首頁視圖以顯示所有文章，並更新 URL 配置以包含首頁路由

--- 02mblog/mblog/mblog/urls.py
+++ 02mblog/mblog/mblog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mysite.views import homepage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage, name='homepage'),
+    # path('post/<slug:slug>/', showpost, name='showpost'),
 ]
--- 02mblog/mblog/mysite/views.py
+++ 02mblog/mblog/mysite/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from mysite.models import Post
 
 # Create your views here.
+def homepage(request):
+    posts = Post.objects.all()
+    post_list = list()
+    for count, post in enumerate(posts):
+        post_list.append(f"Post {count}: {post} <br> {post.body} <br>")
+        print(post)
+    return HttpResponse(post_list)    
+    # return HttpResponse("Hello World!")
+    # return render(request, 'homepage.html', {'posts': Post.objects.all()})
