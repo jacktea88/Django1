b274b265c8e9455a646817c118d8fd2e7f50abcb
jacktea88 <jacktea@gmail.com>
2025-08-22 16:55:04 +0800

    使用plotly繪長條圖

--- 09chart/chart/chart/urls.py
+++ 09chart/chart/chart/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from mysite.views import votes
+from mysite.views import votes, plotly
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('votes/', votes, name='votes'),
+    path('plotly/', plotly, name='plotly'),
 ]
--- 09chart/chart/mysite/views.py
+++ 09chart/chart/mysite/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from mysite.models import Vote
+from plotly.offline import plot
+import plotly.graph_objs as go
+# import numpy as np
 
 # Create your views here.
 def votes(request):
     data = Vote.objects.all()
     # data = Vote.objects.all().order_by('name')
     return render(request, 'votes.html', locals())
+
+def plotly(request):
+    data = Vote.objects.all()
+    # data = Vote.objects.all().order_by('name')
+    x = [d.votes for d in data]
+    y = [d.name for d in data][::-1]
+    trace = go.Bar(x=x, y=y, name='2022', orientation='h')
+    layout = go.Layout(title='2022年高雄市左楠區市議員選舉得票數',
+                       height=500)
+    fig = go.Figure(data=[trace], layout=layout)
+    plot_div = plot(fig, output_type='div')
+    return render(request, 'plotly.html', locals())
