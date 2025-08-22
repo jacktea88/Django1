82762f6040ce0643a1327707998da590b8445b42
jacktea88 <jacktea@gmail.com>
2025-08-22 16:01:26 +0800

    使用chart.js繪製圖表

--- 09chart/chart/mysite/models.py
+++ 09chart/chart/mysite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Vote(models.Model):
+    name = models.CharField(max_length=20)
+    no = models.IntegerField()
+    sex = models.BooleanField(default=False)
+    byear = models.IntegerField()
+    party = models.CharField(max_length=20)
+    votes = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+    
--- 09chart/chart/mysite/views.py
+++ 09chart/chart/mysite/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from mysite.models import Vote
 
 # Create your views here.
+def votes(request):
+    data = Vote.objects.all()
+    # data = Vote.objects.all().order_by('name')
+    return render(request, 'votes.html', locals())
