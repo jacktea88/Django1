77405957926d60dd6d32e95f6e14bda33ccfcae2
jacktea88 <jacktea@gmail.com>
2025-08-28 23:28:47 +0800

    12mshop範例檔from dj4ch12，注意有幾處須修改，搜尋need modify字串，原requirements-origin.txt內的版本太舊，pip install會有問題，目前這版採用最新版，已可運行，requirement.txt是最新版本

--- 12mshop/mshop/mysite/admin.py
+++ 12mshop/mshop/mysite/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from mysite.models import Product, Category
+from mysite import models
 
-# Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('sku', 'name', 'category', 'price', 'stock')
+    list_display = ('category', 'sku', 'name', 'stock', 'price')
     ordering = ('category',)
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Category)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Category)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
