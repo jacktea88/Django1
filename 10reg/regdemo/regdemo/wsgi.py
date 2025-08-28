f0554ace5aa90a0862d41826bd7ea48fcdf21a99
jacktea88 <jacktea@gmail.com>
2025-08-28 12:04:30 +0800

    change folder name to regdemo

--- 10reg/regdemo/regdemo/wsgi.py
+++ 10reg/regdemo/regdemo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dj4ch10 project.
+WSGI config for regdemo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj4ch10.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'regdemo.settings')
 
 application = get_wsgi_application()
