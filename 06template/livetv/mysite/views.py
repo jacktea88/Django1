461498b075f4c284f3ccc8692cb5ffcd7e1c4b89
jacktea88 <jacktea@gmail.com>
2025-09-03 15:52:53 +0800

    基本all in one index.html

--- 06template/livetv/mysite/views.py
+++ 06template/livetv/mysite/views.py
@@ -1,3 +1,8 @@
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    msg = 'Hello, LiveTV'
+    now = datetime.now()
+    return render(request, 'index.html', locals())
