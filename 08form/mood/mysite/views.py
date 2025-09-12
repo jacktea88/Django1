07963f893b037d296bbff0158013f971cf9cb8d7
jacktea88 <jacktea@gmail.com>
2025-09-12 12:09:02 +0800

    接收登入的表單資料及驗證

--- 08form/mood/mysite/views.py
+++ 08form/mood/mysite/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    try:
+        user_id = request.GET['user_id']
+        user_pass = request.GET['user_pass']
+        # user_post = request.GET['user_post']
+        # user_mood = request.GET['mood']
+    except:
+        user_id = None
+    if user_id == 'admin' and user_pass == '12345678':
+        verified = True
+    else:
+        verified = False
+    # print(user_id, verified)    
+    print(user_id, user_pass, verified)    
     return render(request, 'index.html', locals())
