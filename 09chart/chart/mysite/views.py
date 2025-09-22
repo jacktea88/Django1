382f0c025bd4f4eca91f824a768e721467d66c69
jacktea88 <jacktea@gmail.com>
2025-09-22 12:28:35 +0800

    09-測試cookie是否有效

--- 09chart/chart/mysite/views.py
+++ 09chart/chart/mysite/views.py
@@ -7,6 +7,12 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 
 def index(request):
+    if request.session.test_cookie_worked():
+        request.session.delete_test_cookie()
+        message = 'Cookie 已成功啟用'
+    else:
+        message = 'Cookie 啟用失敗'
+        request.session.set_test_cookie()
     return render(request, 'index.html', locals())
 
 
